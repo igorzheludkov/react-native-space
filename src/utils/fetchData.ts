9b40a64b913bc30afb05a3dc18d1edd9c611b470
Ihor Zheludkov <500griven@gmail.com>
2023-05-23 11:18:50 +0300

    loading page indication

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,5 +1,14 @@
 export default async function fetchData(url: string) {
-  const response = await fetch(url, { next: { revalidate: 120 } })
+  const response = await fetch(url, {
+    next: { revalidate: 120 },
+    headers: {
+      Accept: 'application/json'
+    }
+  })
+  if (!response.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
   const data = await response.json()
   return data
 }
